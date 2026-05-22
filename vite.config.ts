d4d9054717600178ea53222e555a9777b5dfc12d
NoahLE <git@noahle.com>
2026-05-21 20:07:23 -0500

    feat(site): added support for GitHub pages
    - Moved dependencies from dev to general section so `--prod` flag can be used
    - Updated vite config so base path exists
    - Added a deployment action file
    - Added GitHub actions extension to VSCode recommendations

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/llamanomicon/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
