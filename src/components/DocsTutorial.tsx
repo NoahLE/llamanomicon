21f5a5ac5ab766ed54dce46f60bbbceaffc2cd47
NoahLE <git@noahle.com>
2026-05-21 20:46:51 -0500

    fix(tutorial): added links to tutorial files
    - Added links to tutorials
    - Increased font size of tutorials and sources

--- src/components/DocsTutorial.tsx
+++ src/components/DocsTutorial.tsx
@@ -1,21 +1,32 @@
+import { Link } from "@heroui/react";
+import { ExternalLinkIcon } from "lucide-react";
+
 export function DocsTutorial() {
   const tutorials = [
     {
       name: "Used car researcher",
+      link: "https://github.com/NoahLE/llamanomicon/blob/main/tutorials/used-car.md",
     },
     {
-      name: "Recipe planner",
+      name: "Recipe maker",
+      link: "https://github.com/NoahLE/llamanomicon/blob/main/tutorials/recipe-maker.md",
     },
     {
       name: "Data extractor",
+      link: "https://github.com/NoahLE/llamanomicon/blob/main/tutorials/food-parser.md",
     },
   ];
 
   return (
-    <ol>
-      {tutorials.map((tut) => (
-        <li key={tut.name}>{tut.name}</li>
+    <ul>
+      {tutorials.map((tutorial) => (
+        <li key={tutorial.name} className="flex flex-row items-center">
+          <ExternalLinkIcon className="mr-1" size={15} />
+          <Link href={tutorial.link} target="_blank" className="text-lg">
+            {tutorial.name}
+          </Link>
+        </li>
       ))}
-    </ol>
+    </ul>
   );
 }
